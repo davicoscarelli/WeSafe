62ed8d70a6600ab7258870990b50c99fd908609d
Davi Coscarelli <davicoscarelli@gmail.com>
2021-01-20 21:26:53 -0400

    bot

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,39 +1,37 @@
 <template>
   <q-page class="">
      <q-scroll-area ref="chatArea" class="chat-area">
-      <q-chat-message
-        :key="message.id" v-for="message in messages"
-        :text="[message.body]"
-        :sent="message.author == 'bot' ?  false : true"
-        :bg-color="message.author == 'bot' &&  'primary'"
-        :text-color="message.author == 'bot' &&  'white'"
-        class="q-px-md q-pt-sm"
-      />
-      <!-- <p :key="message.id" v-for="message in messages" class="message q-ma-md">
-        {{ message.body }}
-      </p> -->
+        <q-chat-message
+          :key="message.id" v-for="message in messages"
+          :text="[message.body]"
+          :sent="message.author == 'bot' ?  false : true"
+          :bg-color="message.author == 'bot' ? 'secondary' : '#e0e0e0'"
+          :text-color="message.author == 'bot' ?  'white' : 'black'"
+          class="q-px-md q-pt-sm"
+          :class="{ 'message-out': message.author === 'you', 'message-in': message.author !== 'you' }"
+        />
       </q-scroll-area>
 
-     <q-footer elevated>
-      <q-toolbar>
-        <q-form @submit="sendMessage('out')" class="full-width">
-          <q-input
-            ref="newMessage"
-            bg-color="white"
-            rounded
-            v-model="youMessage"
-            placeholder="Message"
-            class="full-width"
-            dense
-            outlined
-          >
-            <template>
-              <q-btn round dense flat type="submit" icon="send" color="primary" />
-            </template>
-          </q-input>
-        </q-form>
-      </q-toolbar>
-    </q-footer>
+      <q-footer elevated>
+        <q-toolbar>
+          <q-form @submit="sendMessage('out')" class="full-width">
+            <q-input
+              ref="newMessage"
+              bg-color="white"
+              rounded
+              v-model="youMessage"
+              placeholder="Message"
+              class="full-width"
+              dense
+              outlined
+            >
+              <template>
+                <q-btn round dense flat type="submit" icon="send" color="primary" />
+              </template>
+            </q-input>
+          </q-form>
+        </q-toolbar>
+      </q-footer>
   </q-page>
 </template>
 
@@ -41,8 +39,9 @@
 export default {
   name: 'PageIndex',
   data: () => ({
-    bobMessage: '',
+    botMessage: '',
     youMessage: '',
+    history: [],
     messages: [
       {
         id: 0,
@@ -51,27 +50,114 @@ export default {
       },
     ]
   }),
+  computed: {
+    user(){
+      return { name: 'Davi', age: 19}
+    }
+  },
   methods: {
+
+    postMessage(){
+
+    },
+
+    bot(){
+      let text = ''
+      const input = JSON.stringify(this.history)
+      console.log(input, "inputtt")
+
+      if (this.history[0] == 0){
+        if (input == '[0]') { text = "A vítima é você ou outra pessoa?"}
+        else if (input == '[0,0]') { text = "A vítima está com dificuldade para respirar?"}
+        else if (input == '[0,0,0]') {text = "A vítima tem mais de 8 anos de idade?"}
+        else if (input == '[0,0,1]') {text = "A vítima está consciente?"}
+        else if (input == '[0,0,1,0]') {text = "A vítima tem mais de 8 anos de idade??"}
+        else if (input == '[0,0,1,1]') {text = "A vítima tem mais de 8 anos de idade??"}
+        else if (input == '[0,0,0,1]') {text = "ANSWER1"}
+        else if (input == '[0,0,0,0]') {text = "ANSWER2"}
+        else if (input == '[0,0,1,0,0]') {text = "ANSWER6"}
+        else if (input == '[0,0,1,0,1]') {text = "ANSWER5"}
+        else if (input == '[0,0,1,1,0]') {text = "ANSWER2"}
+        else if (input == '[0,0,1,1,1]') {text = "A vítima é Grávida/Obesa?"}
+        else if (input == '[0,0,1,1,1,0]') {text = "ANSWER3"}
+        else if (input == '[0,0,1,1,1,1]') {text = "ANSWER4"}
+
+        else if (input === '[0,1]') {text = "Você está com dificuldade para respirar?"}
+        else if (input == '[0,1,0]') { 
+          if (this.user.age.length == 0){
+            
+          }else {
+            if (this.user.age > 8){
+              this.history.push(1)
+              text = "ANSWER1"
+            }
+            else{
+              this.history.push(1)
+              text = "ANSWER2+"
+
+            }
+          }
+        }
+        else if (input === '[0,1,1,0]') {text = "Você está sozinho(a)?"}
+        else if (input === '[0,1,1,0]') {text = "ANSWER3.2"}
+        else if (input === '[0,1,1,0,0]') {text = "ANSWER2+"}
+        else if (input === '[0,1,1,0,1]') {text = "CALL 911"}
+    
+      }else if (input[0] == 1){
+          text = 'other.1'
+      }
+      console.log("oh o bot", text)
+      this.botMessage = text
+
+      this.sendMessage('in')
+
+      
+    },
+
+    async createHistory(message){
+      console.log("Hmessage",message)
+
+      if (message === 'no'){
+        this.history.push(0)
+      }
+      else if (message === 'yes'){
+        this.history.push(1)
+      }
+
+      if (message === 'chocking'){
+        this.history.push(0)
+      }
+
+      if (message === 'me'){
+        this.history.push(1)
+      }else if (message === 'other'){
+        this.history.push(0)
+      }
+      
+    },
+
     sendMessage(direction) {
-      if (!this.youMessage && !this.bobMessage) {
+      if (!this.youMessage && !this.botMessage) {
         return
       }
       if (direction === 'out') {
-        this.messages.push({body: this.youMessage, author: 'you'})
+        this.messages.push({body: this.youMessage, author: 'you', id: this.messages.length})
+        this.createHistory(this.youMessage)
         this.youMessage = ''
+        this.bot()
       } else if (direction === 'in') {
-        this.messages.push({body: this.bobMessage, author: 'bot'})
-        this.bobMessage = ''
-      } else {
-        alert('something went wrong')
-      }
-      
-        let messageDisplay = this.$refs.chatArea
-        messageDisplay.setScrollPosition(messageDisplay.$el.scrollHeight, 1);
+        this.messages.push({body: this.botMessage, author: 'bot', id: this.messages.length})
+        this.botMessage = ''
+      } 
+      this.$refs.newMessage.focus();
+      let messageDisplay = this.$refs.chatArea
+      messageDisplay.setScrollPosition(messageDisplay.$el.scrollHeight, 1);
       
     },
+
     clearAllMessages() {
       this.messages = []
+      this.history = []
     }
   }
 }
@@ -90,20 +176,12 @@ export default {
     background: white;
     box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.3)
   }
-  .message {
-    width: 100%;
-    border-radius: 10px;
-    padding: .5em;
-  /*   margin-bottom: .5em; */
-    font-size: 1em;
-  }
+ 
   .message-out {
-    background: #407FFF;
-    color: white;
+    padding-left: 15%;
   }
   .message-in {
-    background: #F1F0F0;
-    color: black;
+    padding-right: 15%;
   }
   .chat-inputs {
     display: flex;

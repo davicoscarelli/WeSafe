776bfcf4ee8747ec5224ecda6d0c4146066296d0
Davi Coscarelli <davicoscarelli@gmail.com>
2021-01-24 11:39:24 -0400

    apple compatible

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -52,7 +52,7 @@
               </template>
             </q-input>
             <div class="col-1 flex flex-center">
-              <q-btn v-if="!recording" round dense flat @click="speechToText" icon="mic" color="white"  />
+              <!-- <q-btn v-if="!recording" round dense flat @click="speechToText" icon="mic" color="white"  /> -->
               <q-spinner-bars v-if="recording" color="white" size="2em"/>
             </div>
             </div>
@@ -64,10 +64,7 @@
 
 <script>
 const sdk = require("microsoft-cognitiveservices-speech-sdk");
-import VueRecord from '@codekraft-studio/vue-record'
-import Vue from 'vue'
 
-Vue.use(VueRecord)
 
 export default {
   
@@ -94,36 +91,36 @@ export default {
   },
   mounted(){
     this.botMessage = 'Welcome to WeSafe! Whats your emergency?'
-    this.audioContext = new AudioContext()
+    // this.audioContext = new AudioContext()
     this.synthesizeSpeech(this.botMessage)
 
   },
   methods: {
 
-    speechToText(){
-      try {
+    // speechToText(){
+    //   try {
   
-        const speechConfig = sdk.SpeechConfig.fromSubscription("80336464dd984e3489ab38cbb895823e", "eastus");
-        let audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
-        let recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
-        const phraseList = sdk.PhraseListGrammar.fromRecognizer(recognizer);
-        phraseList.addPhrase(['choking','no','yes','clear','me','other','someone','else']);
+    //     const speechConfig = sdk.SpeechConfig.fromSubscription("80336464dd984e3489ab38cbb895823e", "eastus");
+    //     let audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
+    //     let recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+    //     const phraseList = sdk.PhraseListGrammar.fromRecognizer(recognizer);
+    //     phraseList.addPhrase(['choking','no','yes','clear','me','other','someone','else']);
         
-        console.log('Speak into your microphone.');
-        this.recording = true
-        recognizer.recognizeOnceAsync(result => {
-            this.youMessage = result.text
-            console.log(`RECOGNIZED: Text=${result.text}`);
-            this.recording = false
-            this.sendMessage('out')
-            this.showOptions = false 
-            return
-        });
+    //     console.log('Speak into your microphone.');
+    //     this.recording = true
+    //     recognizer.recognizeOnceAsync(result => {
+    //         this.youMessage = result.text
+    //         console.log(`RECOGNIZED: Text=${result.text}`);
+    //         this.recording = false
+    //         this.sendMessage('out')
+    //         this.showOptions = false 
+    //         return
+    //     });
         
-      } catch (error) {
-        console.log("recognition", error)
-      }
-    },
+    //   } catch (error) {
+    //     console.log("recognition", error)
+    //   }
+    // },
 
     
     async synthesizeSpeech(text) {
@@ -160,9 +157,9 @@ export default {
           });
     },
 
-    postMessage(){
+    // postMessage(){
 
-    },
+    // },
 
     optionSend(message){
       this.youMessage = message
@@ -293,7 +290,6 @@ export default {
         this.messages.push({body: this.botMessage, author: 'bot', id: this.messages.length})
         this.botMessage = ''
       } 
-      // this.$refs.newMessage.focus();
       let messageDisplay = this.$refs.chatArea
       messageDisplay.setScrollPosition(messageDisplay.$el.scrollHeight + 1000, 1);
       
